7dcaa0038ccb415dadf6de9bf001961ea1a74ab2
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-27 01:46:19 -0600

    Bump version: 0.2.18 → 0.2.19

--- arr_mcp/bazarr_mcp.py
+++ arr_mcp/bazarr_mcp.py
@@ -35,7 +35,7 @@ from agent_utilities.middlewares import (
     JWTClaimsLoggingMiddleware,
 )
 
-__version__ = "0.2.18"
+__version__ = "0.2.19"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
