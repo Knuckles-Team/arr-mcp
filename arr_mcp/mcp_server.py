d954b44e3f2cc4962a35b4c97d8387299cf7419a
knucklessg1 <knucklessg1@gmail.com>
2026-06-06 02:00:45 -0500

    Bump version: 0.39.0 → 0.40.0

--- arr_mcp/mcp_server.py
+++ arr_mcp/mcp_server.py
@@ -43,7 +43,7 @@ from pydantic import Field
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
-__version__ = "0.39.0"
+__version__ = "0.40.0"
 
 logger = get_logger(name="ArrMCP")
 logger.setLevel(logging.INFO)
